80d9af48f33a6020ea4544c02b61e90acf966b87
hptruong93 <hptruong93@gmail.com>
2015-11-19 17:22:37 -0500

    Fixed receiver, now reading from rx fifo only when rx bytes is >0 --working great now

--- Project/MeasuringBoard/Base Project/src/main.c
+++ Project/MeasuringBoard/Base Project/src/main.c
@@ -24,42 +24,9 @@ void write_the_value(uint8_t value, uint8_t addr) {
 }
 
 void write_config() {
-	write_the_value(VAL_CC2500_IOCFG2, CC2500_IOCFG2_REG_ADDR);
-	write_the_value(VAL_CC2500_IOCFG0, CC2500_IOCFG0_REG_ADDR);
-	write_the_value(VAL_CC2500_FIFOTHR, CC2500_FIFOTHR_REG_ADDR);
-	write_the_value(VAL_CC2500_PKTLEN, CC2500_PKTLEN_REG_ADDR);
-	write_the_value(VAL_CC2500_PKTCTRL1, CC2500_PKTCTRL1_REG_ADDR);
-	write_the_value(VAL_CC2500_PKTCTRL0, CC2500_PKTCTRL0_REG_ADDR);
-	write_the_value(VAL_CC2500_ADDR, CC2500_ADDR_REG_ADDR);
-	write_the_value(VAL_CC2500_CHANNR, CC2500_CHANNR_REG_ADDR);
-	write_the_value(VAL_CC2500_FSCTRL1, CC2500_FSCTRL1_REG_ADDR);
-	write_the_value(VAL_CC2500_FSCTRL0, CC2500_FSCTRL0_REG_ADDR);
-	write_the_value(VAL_CC2500_FREQ2, CC2500_FREQ2_REG_ADDR);
-	write_the_value(VAL_CC2500_FREQ1, CC2500_FREQ1_REG_ADDR);
-	write_the_value(VAL_CC2500_FREQ0, CC2500_FREQ0_REG_ADDR);
-	write_the_value(VAL_CC2500_MDMCFG4, CC2500_MDMCFG4_REG_ADDR);
-	write_the_value(VAL_CC2500_MDMCFG3, CC2500_MDMCFG3_REG_ADDR);
-	write_the_value(VAL_CC2500_MDMCFG2, CC2500_MDMCFG2_REG_ADDR);
-	write_the_value(VAL_CC2500_MDMCFG1, CC2500_MDMCFG1_REG_ADDR);
-	write_the_value(VAL_CC2500_MDMCFG0, CC2500_MDMCFG0_REG_ADDR);
-	write_the_value(VAL_CC2500_DEVIATN, CC2500_DEVIATN_REG_ADDR);
-	write_the_value(VAL_CC2500_MCSM1, CC2500_MCSM1_REG_ADDR);
-	write_the_value(VAL_CC2500_MCSM0, CC2500_MCSM0_REG_ADDR);
-	write_the_value(VAL_CC2500_FOCCFG, CC2500_FOCCFG_REG_ADDR);
-	write_the_value(VAL_CC2500_BSCFG, CC2500_BSCFG_REG_ADDR);
-	write_the_value(VAL_CC2500_AGCCTRL2, CC2500_AGCCTRL2_REG_ADDR);
-	write_the_value(VAL_CC2500_AGCCTRL1, CC2500_AGCCTRL1_REG_ADDR);
-	write_the_value(VAL_CC2500_AGCCTRL0, CC2500_AGCCTRL0_REG_ADDR);
-	write_the_value(VAL_CC2500_FREND1, CC2500_FREND1_REG_ADDR);
-	write_the_value(VAL_CC2500_FREND0, CC2500_FREND0_REG_ADDR);
-	write_the_value(VAL_CC2500_FSCAL3, CC2500_FSCAL3_REG_ADDR);
-	write_the_value(VAL_CC2500_FSCAL2, CC2500_FSCAL2_REG_ADDR);
-	write_the_value(VAL_CC2500_FSCAL1, CC2500_FSCAL1_REG_ADDR);
-	write_the_value(VAL_CC2500_FSCAL0, CC2500_FSCAL0_REG_ADDR);
-	write_the_value(VAL_CC2500_FSTEST, CC2500_FSTEST_REG_ADDR);
-	write_the_value(VAL_CC2500_TEST2, CC2500_TEST2_REG_ADDR);
-	write_the_value(VAL_CC2500_TEST1, CC2500_TEST1_REG_ADDR);
-	write_the_value(VAL_CC2500_TEST0, CC2500_TEST0_REG_ADDR);
+	for (uint8_t i = 0; i < CC2500_CONFIG_COUNT; i++) {
+		write_the_value(cc2500_config[i][0], cc2500_config[i][1]);
+	}
 }
 
 int main() {
@@ -83,10 +50,18 @@ int main() {
 		while (!system_ticks);
 		led_rotation_rotate_leds();
 		
-		CC2500_Read(test, 0x35, 2); //SRX
-		CC2500_Read(test + 2, 0x3f, 1); //Read rx fifo
+		CC2500_Read(test, 0x34, 1); //SRX
+		CC2500_Read(test + 1, 0x35, 2); //MARCSTATE
+		CC2500_Read(test + 3, 0x3b, 2); //Rx bytes
+		
+		if (*(test + 3) > 0) {
+			CC2500_Read(test + 5, 0x3f, 1); //Read rx fifo
+			print_buffer(test, 7);
+		} else {
+			//print_buffer(test, 5);
+		}
 
-		print_buffer(test, 4);
+		
 		
 		system_ticks = 0;
 	}
