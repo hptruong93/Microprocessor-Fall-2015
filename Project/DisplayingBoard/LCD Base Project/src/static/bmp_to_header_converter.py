e2d19b45d955e92e98ff14b855bb22d006583936
hptruong93 <hptruong93@gmail.com>
2015-12-05 15:35:09 -0500

    Added bmp to header converter python script

---
+++ Project/DisplayingBoard/LCD Base Project/src/static/bmp_to_header_converter.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+    mem = []
+    if len(sys.argv) > 1:
+       fname = sys.argv[1]
+    else:
+       fname = 'trottier16.bmp'
+    with open(fname, 'r') as f:
+        byte = f.read(1)
+        while byte != "":
+            mem.append(ord(byte))
+            if len(mem) == 4:
+                print '0x%02x%02x%02x%02x,' % (mem[0], mem[1], mem[2], mem[3]) 
+                mem = []
+            byte = f.read(1)
+    if len(mem) != 0:
+        print "0x%02x%02xffff" % (mem[0], mem[1])
